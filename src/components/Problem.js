454b653ad550c96290b9a7f2512087e60d613490
josephlu2 <jjl55@duke.edu>
2022-07-20 16:31:36 -0400

    added state changes and localstorage saves with checkbox and input field

--- src/components/Problem.js
+++ src/components/Problem.js
@@ -1,16 +1,57 @@
 import React from "react"
+import { useState, useEffect } from "react"
 
 const Problem = (props) => {
+    const [note, setNote] = useState();
+    const [checked, setChecked] = React.useState(false);
+
+    const handleChange = () => {
+        setChecked(!checked);
+        localStorage.setItem(props.id + "c", !checked);
+        console.log(checked);
+      };
+
+    useEffect(() => {
+        setNote(localStorage.getItem(props.id));
+        if (localStorage.getItem(props.id + "c") === "true") {
+            setChecked(true);
+        } else {
+            setChecked(false);
+        }
+        console.log(checked);
+    }, []);
+
+    useEffect(() => {
+        console.log(props.id, checked, '-has changed');
+    }, [checked]);
+
+    useEffect(() => {
+        console.log(props.id, note, '-has changed');
+    }, [note]);
+
+
     return ( 
         <div className="problem">
-
-            <h1 className = "prop">{props.problem}</h1>
-            <p className = "prop">{props.difficulty}</p>
-            <p className = "prop">{props.category}</p>
             <form className = "prop">
-                <input className = "formfield" type="text" placeholder="Enter your answer here" />
+                <input 
+                    type="checkbox"
+                    checked={checked}
+                    onChange={handleChange}
+                />
+                <p className = "prop">{props.problem}</p>
+                <p className = "prop">{props.difficulty}</p>
+                <p className = "prop">{props.category}</p>
+                
+                <input className = "formfield" type="text" value = {note} placeholder="Enter notes here" onChange = {event => setNote(event.target.value)} />
+                
+                <button onClick={e => {
+                    e.preventDefault();
+                    localStorage.setItem(props.id, note);
+                    console.log('saved')
+                    }}>
+                    Save
+                </button>
             </form>
-
         </div>
 
      );
