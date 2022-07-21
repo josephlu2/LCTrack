671f0dd04d296fdda8516b118edb5fbc62a46646
josephlu2 <jjl55@duke.edu>
2022-07-21 00:18:17 -0400

    added mobile/desktop warning

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,25 @@
 import React from "react"
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 
 const Navbar = () => {
+
+    
+
     return ( 
-        <nav>
+        
+        <><BrowserView>
+           <nav>
+            
             <h1>LCTrack</h1>
+            <p class = "message">This site uses localstorage to save notes. This means it will not work in incognito mode or with certain extensions! Thanks!</p>
         </nav>
+        </BrowserView><MobileView>
+        <nav>
+            
+            <h1>LCTrack</h1>
+            <p class = "message">This site does not run properly on mobile devices! Please view on a full-screen desktop!</p>
+        </nav>
+            </MobileView></>
      );
 }
  
